73b3fa2d8e6c996c9f28bc0f1f89feb173928b32
Nayan Bhatia <nbhatia3@ucsc.edu>
2023-05-11 11:02:33 -0700

    Remove comments

--- graph_send_data.py
+++ graph_send_data.py
@@ -44,34 +44,10 @@ def animate(i):
         acc=(yar[-1]-yar[-2])/1.5
         MESSAGE = str.encode("{acceleration},{speed},{time}\n".format(acceleration=acc,speed=yar[-1],time=xar[-1]))
         if(acc<-30):
-            # requests.get("http://localhost/sihcodes/twiliocall/makecall.php?number=918779686105")
             print("second call")
             winsound.Beep(frequency, duration)
         time.sleep(1)
         tcpClientA.send(MESSAGE)
-        
-            # text_send="GAJD6-919987746997-19.123456-72.123456"
-            # mobile_to_send='919004910418'
-            # response=json.loads(requests.get("https://www.smsgatewayhub.com/api/mt/SendSMS?APIKey=eOViacIZGkKbu8bo82s5xg&senderid=SMSTST&channel=2&DCS=0&flashsms=0&number={mobile_to_send}&text={text_send}&route=1".format(text_send=text_send,mobile_to_send=mobile_to_send)).text)
-            # if(response['ErrorCode']=='000' and response['ErrorMessage']=='Success'):
-            #     print("sms successfull")
-            #     response_call=json.loads(requests.get("http://localhost/sihcodes/twiliocall/makecall.php?phone={phone1}".format(phone1='919987746997')).text)
-            #     response_call_var=json.loads(requests.get("http://localhost/sihcodes/twiliocall/makecall.php?phone={phone2}".format(phone2='918779686105')).text)
-
-            #     if(response_call['status']=='SUCCESS'):
-            #         print("call successfull")
-            #         print(response_call['message'])
-            #     else:
-            #         print(response_call['message'])
-            # else:
-            #     winsound.Beep(1000, 500)
-            # requests.get("http://localhost/sihcodes/twiliocall/makecall.php?phone1={phone1}&phone2={phone2}".format(phone1='918779686105',phone2='919987746997'))
-
-
-
-            # os.system("accident.mp3")
-
-
         print(acc)
     except:
         pass
